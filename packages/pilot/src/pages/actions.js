7596d40bfa474ec917b2600bbd26372ac3a87a1f
Ivan Micai da Silva <ivanmicai@gmail.com>
2018-06-28 16:24:43 -0300

    recipients-list: add Route and Actions

--- packages/pilot/src/pages/actions.js
+++ packages/pilot/src/pages/actions.js
@@ -3,12 +3,14 @@ import { combineReducers } from 'redux'
 import account, { epic as accountEpic } from './Account/actions'
 import balance from './Balance'
 import { reducers as transactionsReducers } from './Transactions'
+import { reducers as recipientsReducers } from './Recipients'
 
 export const rootEpic = accountEpic
 
 export const rootReducer = combineReducers({
   account,
   balance,
+  recipients: recipientsReducers.search,
   transactionDetails: transactionsReducers.details,
   transactions: transactionsReducers.search,
 })
